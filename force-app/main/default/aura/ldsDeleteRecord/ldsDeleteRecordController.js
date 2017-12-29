1d1ae33d0338fe205f9e2efb0dc2ac305e680f74
svittala <svittala@hotmail.com>
2017-12-28 22:03:17 -0500

    added Lightning Data Service Components

---
+++ force-app/main/default/aura/ldsDeleteRecord/ldsDeleteRecordController.js
@@ -0,0 +1,27 @@
+({
+    handleDeleteRecord: function(component, event, helper) {
+    component.find("recordHandler").deleteRecord($A.getCallback(function(deleteResult) {
+        if (deleteResult.state === "SUCCESS" || deleteResult.state === "DRAFT") {
+            console.log("Record is deleted.");
+            var resultsToast = $A.get("e.force:showToast");
+            resultsToast.setParams({
+                "title": "Deleted",
+                "message": "The record was deleted."
+            });
+            resultsToast.fire();
+        }
+        else if (deleteResult.state === "INCOMPLETE") {
+            component.set("v.recordError","Are you offline!! Try after coming online");
+            console.log("User is offline, device doesn't support drafts.");
+        }
+        else if (deleteResult.state === "ERROR") {
+            component.set("v.recordError","Error Deleting Contact Admin");
+            console.log('Problem deleting record: ' +
+                        JSON.stringify(deleteResult.error));
+        }
+        else {
+            console.log('Unknown problem, state: ' + deleteResult.state +
+                        ', error: ' + JSON.stringify(deleteResult.error));
+        }
+    }));
+}})
